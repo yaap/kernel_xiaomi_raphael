5cec22c2a566ece64ee4de6f2b361b18639ffc74
kondors1995 <normandija1945@gmail.com>
2024-12-21 21:43:08 +0200

    drivers/staging:remove ksu from source
    As per yaap rules

--- ksu_update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo pulling main branch of KernelSU
-
-git subtree pull --prefix=drivers/staging/kernelsu https://github.com/rifsxd/KernelSU-Next.git next --squash -m "drivers: kernelsu: update"
-
-echo done
